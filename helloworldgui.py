346489e1946272ebabb3d67a6c2f5e19f586d8d7
Mayr08 <mayr97.sm@gmx.at>
2021-12-01 21:42:32 +0100

    hello world gui create

---
+++ helloworldgui.py
@@ -0,0 +1,21 @@
+from Tkinter import *
+import tkFont
+
+win = Tk()
+
+myFont = tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
+
+def exitProgram():
+	print("Exit Button pressed")
+	win.quit()
+
+win.title("First GUI")
+win.geometry('800x480')
+
+exitButton  = Button(win, text = "Exit", font = myFont, command = exitProgram, height =2 , width = 6) 
+exitButton.pack(side = BOTTOM)
+
+ledButton = Button(win, text = "LED ON", font = myFont, command = ledON, height = 2, width =8 )
+ledButton.pack()
+
+mainloop()
