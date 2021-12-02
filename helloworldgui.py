ce8859e348a0c4d835ada34768fec1bd9e88eba5
Mayr08 <mayr97.sm@gmx.at>
2021-12-02 14:47:31 +0100

    created new helloworldgui

---
+++ helloworldgui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk() # Fenster erstellen
+root.wm_title("Raspberry Pi GUI") # Fenster Titel
+root.config(background = "#FFFFFF") # Hintergrundfarbe des Fensters
+ 
+# Hier kommen die Elemente hin
+ 
+root.mainloop() # GUI wird upgedated. Danach keine Elemente setzen
