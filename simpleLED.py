6fed476161031906044c0cd0133b15b8a5433c48
Mayr08 <mayr97.sm@gmx.at>
2021-12-03 11:35:12 +0100

    Update simpleLED.py

--- simpleLED.py
+++ simpleLED.py
@@ -2,16 +2,16 @@ import RPi.GPIO as GPIO
 LED = 21
 
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D21
+#pixel_pin = board.D21
 # The number of NeoPixels
-num_pixels = 24
+#num_pixels = 24
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRB
+#ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
-)
+#pixels = neopixel.NeoPixel(
+#    pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
+#)
 
 
 GPIO.setmode(GPIO.BCM)
